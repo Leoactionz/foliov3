4e9c38c7022def24b8db570fabb44e4102a2d452
LeotheUXBro <leoucu@gmail.com>
2021-10-02 23:19:01 +0100

    Grammar update

--- src/mock/data.js
+++ src/mock/data.js
@@ -22,11 +22,11 @@ export const heroData = {
 export const aboutData = {
   img: 'LeonardProfile.jpg',
   paragraphOne:
-    'Leonard Ugorji is a UX-UI Designer and UI Developer; who unintentionally started his career in the university. Fun fact, he skipped lessons to design on Maya😄.',
+    'Leonard Ugorji is a UX/UI Designer and UI Developer, who unintentionally started his career in the university. Fun fact: He skipped lessons to design on Maya. 😄',
   paragraphTwo:
-    'Leo is currently a UX Designer at Centric Gateway working with the SeerBit Team. He is a founding member of the Inspire Innovative Cohort. He enjoys building high quality applications and he is learning ReactJS at the moment.',
+    'Leo is currently a UX Designer at Centric Gateway working with the SeerBit Team. He is a founding member of the Inspire Innovative Cohort. He enjoys building high-quality applications and is learning ReactJS at the moment.',
   paragraphThree:
-    'Leo likes being challenged especially when it creates that opportunity to learn something new and that is what excites him in this industry. Also contributes to an open source project to proffer solutions. At his leisure, he plays table tennis, basketball, and spends time with his family & friends.',
+    "Leo likes being challenged especially when it creates that opportunity to learn something new—that's what excites him about this industry. He's also a contributor to the ParetoOS open source project. At his leisure, he plays table tennis, basketball, and spends time with his family and friends.",
   resume: 'https://drive.google.com/file/d/1M2tUgH2d9eOw69kcCOWw3746BCuRQIrD/view?usp=sharing', // if no resume, the button will not show up
 };
 
@@ -36,7 +36,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'SeerBit.jpg',
     title: 'Encouraging Payment without boundaries',
-    info: "Here's where I currently build diverse payment features. At SeerBit, payment is seamless and tailored to meet individuals need. ",
+    info: "Here's where he currently build diverse payment features. At SeerBit, payment is seamless and tailored to meet individuals need. ",
     info2: 'UI/UX DESIGN | CUSTOMER-FACING APP',
     url: 'https://seerbit.com/',
     repo: 'https://github.com/seerbit', // if no repo, the button will not show up
@@ -45,7 +45,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'Blinqpay.jpg',
     title: 'Embracing effective and simple ways to attract more customers.',
-    info: "Here's my opinion about landing page which is the first touch point for your brand, product or service.",
+    info: "Here's his opinion about landing page which is the first touch point for your brand, product or service.",
     info2: 'UI/UX DESIGN | CUSTOMER-FACING APP',
     url: 'https://leoucon.medium.com/blinqpay-a-conversion-optimized-landing-page-ui-ux-case-study-d7afbbb64cae',
     // repo: '', // if no repo, the button will not show up
@@ -54,7 +54,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'Unihelt.jpg',
     title: 'Reducing the wait time at the health providers',
-    info: 'Here is how I help patients minimize the time spent waiting to see a doctor.',
+    info: 'Here is how he helps patients minimize the time spent waiting to see a doctor.',
     info2: 'UI/UX DESIGN',
     url: 'https://uxfol.io/project/049dd3ee/Unihelt---Manage-waiting-time-at-hospitals',
     // repo: '', // if no repo, the button will not show up
@@ -63,7 +63,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'UbaOpenAPI.jpg',
     title: 'Enhancing financial services and processes in Africa',
-    info: "Here's I helped the Nigerian pan-African financial services group revolutionize banking APIs",
+    info: "Here's where he helped the Nigerian pan-African financial services group revolutionize banking APIs",
     // info: 'Portfolio of some UI projects I played with since 2019. Including some open source projects where I contributed',
     info2: 'UI/UX DESIGN | CUSTOMER-FACING APP | HTML | CSS | REACT | Bootstrap',
     url: 'https://developer.ubaopenbanking.com/',
