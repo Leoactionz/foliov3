b729d39b7ef568c1613cd1a7007b4646dfac501a
LeotheUXBro <leoucu@gmail.com>
2021-10-03 12:18:45 +0100

    target blank on about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,13 +7,7 @@ import PortfolioContext from '../../context/context';
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
-  const {
-    img,
-    paragraphOne,
-    // paragraphTwo,
-    // paragraphThree,
-    resume,
-  } = about;
+  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -47,27 +41,42 @@ const About = () => {
                   {paragraphOne ||
                     'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
                 </p>
-                <p className="about-wrapper__info-text">
-                  {/* {paragraphTwo || ''} */}
-                  Leo is currently a UX Designer at Centric Gateway working with the SeerBit Team.
-                  He is a founding member of the{' '}
-                  <a href="https://www.linkedin.com/groups/8737901/" className="text-link">
-                    Inspire Innovative Cohort
-                  </a>
-                  . He enjoys building high-quality applications and is learning ReactJS at the
-                  moment.
-                </p>
-                <p className="about-wrapper__info-text">
-                  {/* {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'} */}
-                  Leo likes being challenged especially when it creates that opportunity to learn
-                  something new—that&apos;s what excites him about this industry. He&apos;s also a
-                  contributor to the{' '}
-                  <a href="https://github.com/mikhael28/paretOS" className="text-link">
-                    ParetoOS open source project
-                  </a>{' '}
-                  . At his leisure, he plays table tennis, basketball, and spends time with his
-                  family and friends.
-                </p>
+
+                {paragraphTwo || (
+                  <p className="about-wrapper__info-text">
+                    Leo is currently a UX Designer at Centric Gateway working with the SeerBit Team.
+                    He is a founding member of the
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-link"
+                      href="https://www.linkedin.com/groups/8737901/"
+                    >
+                      Inspire Innovative Cohort
+                    </a>
+                    . He enjoys building high-quality applications and is learning ReactJS at the
+                    moment.
+                  </p>
+                )}
+
+                {paragraphThree || (
+                  <p className="about-wrapper__info-text">
+                    Leo likes being challenged especially when it creates that opportunity to learn
+                    something new—that&apos;s what excites him about this industry. He&apos;s also a
+                    contributor to the{' '}
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-link"
+                      href="https://github.com/mikhael28/paretOS"
+                    >
+                      ParetoOS open source project
+                    </a>{' '}
+                    . At his leisure, he plays table tennis, basketball, and spends time with his
+                    family and friends.
+                  </p>
+                )}
+
                 {resume && (
                   <span className="d-flex mt-3">
                     <a
